899c08ca5a3b9858aa4c83ebfa777e6a47d2385c
Alejandro Salguero <a.salguero.quiros@accenture.com>
2023-06-29 21:05:16 -0600

    Pokemon game css design

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pokemon-container">
+    <img
+      class="pokemon-hidden"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon picture"
+    />
+    <img
+      class="fade-in"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon picture"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'PokemonPicture',
+};
+</script>
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 32%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.pokemon-hidden {
+  filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Quien es este pokemon?</h1>
+        <PokemonPicture />
+        <PokemonOptions />
+    </div>
+</template>
+
+<script lang="ts">
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+
+export default {
+  name: 'PokemonPage',
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+};
+</script>
+
+<style></style>
